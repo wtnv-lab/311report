5ee650c4bfd353d34ea77948c5b36a084475e6df
Hidenori Watanave <hwtnv2006@gmail.com>
2026-02-21 12:54:24 +0900

    Set Mapbox token and finalize config cleanup

--- js/app-config.js
+++ js/app-config.js
@@ -1,17 +1,8 @@
 window.APP_CONFIG = {
-  googleMapsApiKey: "",
-  mapboxAccessToken: "",
+  mapboxAccessToken: "pk.eyJ1IjoiaHd0bnYiLCJhIjoiY21sdnFiaGRtMDltbTNmczV5YW5mdDFlbyJ9.DtU1T9geoBDpAOAMt-7k4g",
   mapboxStyleStreets: "mapbox/dark-v11",
-  mapboxStyleSatellite: "mapbox/satellite-streets-v12",
-  cesiumIonToken: "",
   analyticsTrackingId: "UA-93496169-1",
   analyticsDomain: "auto",
-  mapTilerTerrainKey: "",
   githubUrl: "https://github.com/wtnv-lab/311report/",
-  dataUrl: "data/czml/weathernews.geojson",
   reportGeoJsonUrl: "data/czml/weathernews.geojson",
-  viewPoints: [
-    { label: "日本全国", lat: 34.00934, lng: 135.843524, heading: -47, pitch: -50, range: 2000000 },
-    { label: "初期視点", lat: 37.81995, lng: 141.101672, heading: -27, pitch: -40, range: 240000 },
-  ],
 };
